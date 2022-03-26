9418f7cc960c98be5ae67810fd5e985ce78e6d2f
Victor Torres <vmtorres@stefanini.com>
2022-03-26 15:29:52 -0300

    build: :package: adicionando spring com java 11 ao projeto backend

---
+++ src/test/java/br/com/vsm/VsmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
